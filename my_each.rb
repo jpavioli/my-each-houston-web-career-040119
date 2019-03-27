ae47297f837ddca9d4e02d6daf7ace845377ee78
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 16:11:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,9 +4,12 @@ def my_each(collection)
   collection_size = collection.length 
   if collection_size > 1 
     while counter < collection_size
-    
+      yield collection[i]
     end
   else
-    
+    new collection = collection
   end
+  new_collection
 end
+
+my_each
