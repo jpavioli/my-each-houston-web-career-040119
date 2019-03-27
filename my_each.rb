b2520c1047809e625b5b87498b274f159f956ac9
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 16:04:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,15 @@
 def my_each(collection)
   i = 0
   new_collection = []
-  while i < collection.length do
-   yield collection[i]
-    new_collection << collection[i]
-    i += 1
-  end 
+  if collection.length > 1 
+    while i < collection.length do
+    yield collection[i]
+      new_collection << collection[i]
+      i += 1
+    end
+  else
+    new_collection = collection
+  end
   new_collection
 end
 
