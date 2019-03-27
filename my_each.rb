b0ce5991b23c3615550fadd01ebeee606ccf4e4c
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-27 16:07:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,6 @@
-collection = [1,2,3,4]
-
 def my_each(collection)
-  i = 0
+  counter = 0
   new_collection = []
-  if collection.length > 1 
-    while i < collection.length do
-    yield collection[i]
-      new_collection << collection[i]
-      i += 1
-    end
-  else
-    new_collection = collection
-  end
-  new_collection
+  collection_size = collection.length 
+  
 end
-
-my_each(1) {|x| puts x} 
